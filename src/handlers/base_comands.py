d55b88221c50facceef131fe43db517341803b83
dmitriiybondar <dmitrybondar6@gmail.com>
2026-07-01 01:08:42 +0300

    setup ngrok and nginx

--- src/handlers/base_comands.py
+++ src/handlers/base_comands.py
@@ -1,9 +1,21 @@
 from aiogram import Router
-from aiogram.types import Message
 from aiogram.filters import Command
+from aiogram.types.web_app_info import WebAppInfo
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 router = Router()
 
 @router.message(Command("start"))
-async def start_command(message: Message):
+async def start_cmd(message: Message):
     await message.answer("Бот запущено")
+
+@router.message(Command("site"))
+async def site(message: Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(
+            text="Відкрити сайт",
+            web_app=WebAppInfo(url="https://uncrystallized-daisey-uninterwoven.ngrok-free.dev/")
+        )]
+    ])
+
+    await message.answer("Натисни кноопку", reply_markup=keyboard)
