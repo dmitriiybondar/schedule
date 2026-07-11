e74085fce508cd1d2419273e84e89c98b96f39ac
dmitriiybondar <dmitrybondar6@gmail.com>
2026-07-11 15:42:00 +0300

    create timer page

--- frontend/admin/timescale/app.js
+++ frontend/admin/timescale/app.js
@@ -1,9 +1,18 @@
 backButton();
+addButton();
 
 function backButton() {
     let button = document.querySelector(".back");
 
     button.addEventListener("click", () => {
         window.location.href = "/admin/dashboard/index.html"
+    });
+}
+
+function addButton() {
+    let button = document.querySelector(".add-slot-button");
+
+    button.addEventListener("click", () => {
+        window.location.href = "/admin/timer/index.html"
     });
 }
